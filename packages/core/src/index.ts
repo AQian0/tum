24f5475a41b82d822d6508dbf5b2068d1331f9a3
AQian <970506352@qq.com>
2026-05-20 10:57:39 +0800

    refactor(core): rename createDockSession module to dockSession

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 export type * from "./protocol";
 export { send, onEvent } from "./api";
 export { createSessionTransport } from "./transport";
-export { createDockSession } from "./createDockSession";
-export type { TerminalPaneParams } from "./createDockSession";
+export { createDockSession } from "./dockSession";
+export type { TerminalPaneParams } from "./dockSession";
 export { initSessionStore, getSessionStore } from "@tum/session";
 export type { SessionEntry, SessionTab, SessionTransport } from "@tum/session";
